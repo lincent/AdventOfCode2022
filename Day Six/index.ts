69c91e4f04f00a54eeac9ff181acdf3cad6285f0
Luke <lukevincent2@gmail.com>
2022-12-06 17:31:18 +0000

    convert day 6 to typescript

--- Day Six/index.ts
+++ Day Six/index.ts
@@ -1,17 +1,18 @@
-const fs = require('fs')
-const readline = require('readline');
+import * as fs from 'fs';
+import * as readline from 'readline';
 
+const fileName = process.argv[2];
 const file = readline.createInterface({
-    // input: fs.createReadStream('testData.txt'),
-    input: fs.createReadStream('input.txt'),
+    input: fs.createReadStream(fileName),
     output: process.stdout,
     terminal: false
 });
 
-let partOneScore = '';
-let partTwoScore = '';
+console.log("Running with file " + fileName)
 
-const numbers = '123456789';
+let partOneScore = 0;
+let partTwoScore = 0;
+
 file.on('line', (line) => {
     partOne(line)
     partTwo(line)
@@ -22,23 +23,24 @@ file.on('close', () => {
     console.log("Part Two: " + partTwoScore);
 })
 
-const partOne = (line) => {
+const partOne = (line: string) => {
     partOneScore = calculate(line, 4);
 }
 
-const partTwo = (line) => {
+const partTwo = (line: string) => {
     partTwoScore = calculate(line, 14);
 }
 
-const calculate = (line, bufferLength) => {
+const calculate = (line: string, bufferLength: number) => {
     for (let i = 0; i + bufferLength < line.length; i++) {
         const bufferedCode = line.substring(i, i + bufferLength); 
         
         let set = new Set();
-        [...bufferedCode].map(x => set.add(x));
+        Array.from(bufferedCode).map(x => set.add(x));
 
         if (set.size === bufferLength) {
            return i + bufferLength;
         }
    }
+   return -1;
 }
